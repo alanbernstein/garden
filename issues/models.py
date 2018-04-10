a88b9147e5bb28f2ee19f36b8bcbf9363abec389
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2018-04-09 23:45:36 -0500

    Create some basic model classes

--- issues/models.py
+++ issues/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.CharField(max_length=1000, null=True, blank=True)
+    user = models.ForeignKey(User)
