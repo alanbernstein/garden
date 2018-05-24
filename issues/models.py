3e0b1612f8adec56e8be59dd3e444daa1804053b
Kevin Brown <38253460+brokev03@users.noreply.github.com>
2018-05-23 22:38:52 -0400

    added some lines to models.py

--- issues/models.py
+++ issues/models.py
@@ -5,12 +5,17 @@ from django.db import models
 
 # Create your models here.
 
-
 class User(models.Model):
     name = models.CharField(max_length=300)
-
 
 class Issue(models.Model):
     title = models.CharField(max_length=100)
     details = models.CharField(max_length=1000, null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, realted_name = 'issues', on_delete=models.CASCADE)
+    xcoord = models.FloatField()
+    ycoord = models.FloatField()
+
+class Parcel(models.Model):
+    plot_id = models.IntegerField()
+    gardener = models.ForeignKey(User, null = True, related_name = 'gardners', on_delete=models.CASCADE)
